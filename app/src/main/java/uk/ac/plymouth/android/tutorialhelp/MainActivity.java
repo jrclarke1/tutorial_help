6688fc6fb4eeef93d2f5c0c5cb87e4cf431a0926
John Clarke <johnclarke@outlook.com>
2017-03-17 18:03:58 +0000

    Initial commit of functioning Nearby protoype

--- app/src/main/java/uk/ac/plymouth/android/tutorialhelp/MainActivity.java
+++ app/src/main/java/uk/ac/plymouth/android/tutorialhelp/MainActivity.java
@@ -1,37 +1,76 @@
 package uk.ac.plymouth.android.tutorialhelp;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.net.wifi.p2p.WifiP2pDevice;
-import android.net.wifi.p2p.WifiP2pDeviceList;
-import android.net.wifi.p2p.WifiP2pManager;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageFilter;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import org.w3c.dom.Text;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity
+        implements
+        ConnectionCallbacks,
+        OnConnectionFailedListener
+{
+    private static final String TAG = "MainActivity";
 
-    private final IntentFilter intentFilter = new IntentFilter();
+    //private final IntentFilter intentFilter = new IntentFilter();
+    private GoogleApiClient mGoogleApiClient = null;
+
+    //TODO: Change this to a list
+    //TODO: Method to unpublish ALL messages in the list
+    private Message mActiveMessage = null;
+
+    private MessageListener mMessageListener = null;
+
+    private MessageFilter   mMessageFilter = null;
+
+
+    //GUI Components
+    Button btnSend;
+    TextView txtMessage;
 
 
     protected void onCreate(Bundle savedInstanceState)
     {
+        //Important!
         super.onCreate(savedInstanceState);
+
+        //Connect to Nearby service
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(this)
+                .enableAutoManage(this, this)
+                .build();
+
+        mGoogleApiClient.connect();
+
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.btnDiscoverPeers).setOnClickListener(new View.OnClickListener() {
+        btnSend = (Button) findViewById(R.id.btnSend);
+        txtMessage = (TextView) findViewById(R.id.txtMessage);
+
+        findViewById(R.id.btnSend).setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-
+                publish(txtMessage.getText().toString());
             }
         });
 
@@ -40,16 +79,115 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume()
     {
-
+        super.onResume();
     }
 
     @Override
     protected void onPause()
     {
+        super.onPause();
+    }
+
+
+    private void setupNearbyMessagesAPI()
+    {
+        //TODO: "If enableAutoManage cannot be used: eg) want to live outside
+        //activity lifecycle,
+        // see https://developers.google.com/nearby/messages/android/user-consent
+
+        //Create a message listener
+        mMessageListener = new MessageListener() {
+            @Override
+            public void onFound(Message message)
+            {
+                String messageString = new String(message.getContent());
+                Log.d(TAG, "Message recvd: " + messageString);
+            }
+
+            @Override
+            public void onLost(Message message)
+            {
+                String messageAsString = new String(message.getContent());
+                Log.d(TAG, "Lost sight of message: " + messageAsString);
+            }
+        };
+
+        subscribe();
 
     }
 
-    
+    @Override
+    public void onConnected(@Nullable Bundle bundle)
+    {
+        setupNearbyMessagesAPI();
+    }
 
+    @Override
+    public void onConnectionSuspended(int i)
+    {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult)
+    {
+
+    }
+
+    /**
+     * Send a message
+     * @param message
+     */
+    private void publish(String message)
+    {
+        //TODO: add to messages list
+        Message activeMessage = new Message(message.getBytes());
+        mActiveMessage = activeMessage;
+        Nearby.Messages.publish(mGoogleApiClient, activeMessage);
+
+    }
+
+
+    //TODO:
+    private void unpublish()
+    {
+        if (mActiveMessage != null)
+        {
+            Nearby.Messages.unpublish(mGoogleApiClient, mActiveMessage);
+        }
+
+    }
+
+    //TODO:
+    private void unpublishMostRecent()
+    {
+
+    }
+
+    //TODO:
+    private void unpublishAll()
+    {
+
+    }
+
+    private void subscribe()
+    {
+        Log.i(TAG, "Subscribing.");
+        Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener);
+
+
+        //TODO: Use subscribe options: includes useful stuff like messageFilter
+        //TODO: subscribe option SubscribeCallback
+        //TODO: subscribe option ResultCallback..... https://developers.google.com/nearby/messages/android/pub-sub
+        // SubscribeOptions options = new SubscribeOptions(Strategy.DEFAULT)
+
+        // Nearby.Messages.subscribe(mGoogleApiClient, mMessageListener, options);
+    }
+
+    private void unSubscribe()
+    {
+        Log.i(TAG, "Unsubscribing.");
+        Nearby.Messages.unsubscribe(mGoogleApiClient, mMessageListener);
+    }
 
 }
