eb096e231ab029c938dab1481932eb8cf0c62fa3
John Clarke <johnclarke@outlook.com>
2017-04-20 12:25:16 +0100

    Set theme correctly

--- app/src/main/java/uk/ac/plymouth/android/tutorialhelp/MainActivity.java
+++ app/src/main/java/uk/ac/plymouth/android/tutorialhelp/MainActivity.java
@@ -1,9 +1,10 @@
 package uk.ac.plymouth.android.tutorialhelp;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -19,7 +20,7 @@ import com.google.android.gms.nearby.messages.MessageFilter;
 import com.google.android.gms.nearby.messages.MessageListener;
 
 
-public class MainActivity extends FragmentActivity
+public class MainActivity extends AppCompatActivity
         implements
         ConnectionCallbacks,
         OnConnectionFailedListener
@@ -52,7 +53,6 @@ public class MainActivity extends FragmentActivity
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Nearby.MESSAGES_API)
                 .addConnectionCallbacks(this)
-                .enableAutoManage(this, this)
                 .build();
 
         mGoogleApiClient.connect();
---
+++ app/src/main/java/uk/ac/plymouth/android/tutorialhelp/StartActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.plymouth.android.tutorialhelp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class StartActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+}
